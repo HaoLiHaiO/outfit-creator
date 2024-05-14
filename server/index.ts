9eee12a60cf7a9166d53b70307cf37875554c71c
HaoLiHaiO <pinelwebdevelopment@gmail.com>
2024-05-14 17:37:48 +0300

    feat: use api endpoint in Express app

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import cors from 'cors';
-import router from './routes';
+import setupRoutes from './routes';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(router);
+setupRoutes(app);
 
 app.listen(5000, () => {
     console.log('Server started properly and it is running on port 5000');
